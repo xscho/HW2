902241285b7432b07a745e8e109df86fb6aedf3d
xscho <lina.che.19@bk.ru>
2024-03-30 13:10:12 +0300

    Добавьте файлы проекта.

---
+++ HW2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    internal class Program
+    {
+        class MainClass
+        {
+            static void Main(string[] args)
+            {
+                string modif;
+                Console.WriteLine("Напишите что-то");
+                var str = Console.ReadLine();
+
+                Console.WriteLine("Укажите глубину эха");
+                var deep = int.Parse(Console.ReadLine());
+
+                Echo(str, deep);
+
+                Console.ReadKey();
+            }
+
+            static void Echo(string saidworld, int deep)
+            {
+                var modif = saidworld;
+
+                if (modif.Length > 2)
+                {
+                    modif = modif.Remove(0, 2);
+                }
+
+                Console.WriteLine("..." + modif);
+
+                if (deep > 1)
+                {
+                    Echo(modif, deep - 1);
+                }
+            }
+        }
+    }
+}
